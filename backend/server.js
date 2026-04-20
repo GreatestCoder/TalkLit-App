ab91af1c214f3b0d727b453781b42adb7b2b64e0
GreatestCoder <namansharma9858581009@gmail.com>
2026-04-20 22:32:25 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config();
-const app = express();
+const path = require("path");
 const auth_router = require("./routes/auth_router.js");
 const message_router = require("./routes/message_router.js");
 
+dotenv.config();
+const app = express();
+const __dirname__ = path.resolve();
+
+
 app.use("/api/auth", auth_router);
 app.use("/api/messages", message_router);
+
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname__, "../frontend/dist")));
+
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname__, "../frontend", "dist", "index.html"));
+    });
+}
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
